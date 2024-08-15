86bd970535d1077c1f4ee859d87cc6859452024d
John Njoku <mostnjoku@yahoo.com>
2024-08-15 21:29:02 +0100

    Initial commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <h6 className="text-sm text-center w-full">
+      Created via 💙{" "}
+         </h6>
+  );
+}
+
+export default Footer;
